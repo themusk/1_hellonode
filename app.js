20d86d31745ece43bd969ff85934a737f2ded929
piomak <majchrzyk23@gmail.com>
2015-10-30 17:48:19 +0100

    rollback

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var PORT = process.env.PORT || DEFAULT_PORT;
 // App
 var app = express();
 app.get('/', function (req, res) {
-  res.send('Hello Automated build\n');
+  res.send('Hello world\n');
 });
 
 app.listen(PORT)
